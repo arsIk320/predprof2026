418204604556863547e532599d30acc0ca62b4d1
arsIk320 <154743587+arsIk320@users.noreply.github.com>
2025-12-22 01:28:12 +0300

    День 1
    Потихоньку отстраиваю страничку регистрации + вспоминаю sqlalchemy

--- app.py
+++ app.py
@@ -14,7 +14,18 @@ migrate = Migrate(app, db)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(150), unique=True)
+    first_name = db.Column(db.String(150))
+    middle_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
+    phone_number = db.Column(db.String(150))
+    date_of_birth = db.Column(db.String(150))
+    role = db.Column(db.String(150))
+    admin = db.Column(db.Integer)
+    password = db.Column(db.String(150))
+
+
 
 @app.route("/main")
 def main():
@@ -32,9 +43,43 @@ def entry():
         
     return render_template('entry_page.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('entry_page.html')
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        middle_name = request.form.get('middle_name')
+        date_of_birth = request.form.get('dob')
+        category = request.form.get('category')
+        login = request.form.get('login')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        phone_number = request.form.get('phone_number')
+
+        user_by_email = Users.query.filter_by(email=email).first()
+        user_by_login = Users.query.filter_by(login=login).first()
+
+        if not user_by_email and not user_by_login:
+            new_user = Users(
+                login = login,
+                first_name = first_name,
+                middle_name = middle_name,
+                last_name = last_name,
+                email = email,
+                phone_number = phone_number,
+                date_of_birth = date_of_birth,
+                role = category,
+                password = password
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+        return redirect(url_for('register'))
+
+
+
+    return render_template('register_page.html')
 
 if __name__ == "__main__":
     with app.app_context():
